5491175496548af74ab062bcab4482b61d93a59c
Mary Muensterman <mcmuensterman@yahoo.com>
2022-05-03 18:09:27 -0500

    created user entity

--- GameyMcThingy.WebAPI/Program.cs
+++ GameyMcThingy.WebAPI/Program.cs
@@ -6,8 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 // Add connection string and DbContext setup
-9          var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-10        builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+          var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+            builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
