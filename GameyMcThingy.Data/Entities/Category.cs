34f043f278383af6ea0baef6d6ed773bf244ec31
Mary Muensterman <mcmuensterman@yahoo.com>
2022-05-12 18:06:51 -0500

    started catergories

--- GameyMcThingy.Data/Entities/Category.cs
+++ GameyMcThingy.Data/Entities/Category.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace GameyMcThingy.Data.Entities
 {
     public partial class Category
     {
+        [Key]
         public int CategoryId { get; set; }
+        [Required]
         public int? GameId { get; set; }
+        [Required]
         public string? GameCategory { get; set; }
-
+        [Required]
         public virtual Game? Game { get; set; }
     }
 }
