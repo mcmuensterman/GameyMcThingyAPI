a1a15f7f6a90ae98bdaa93a0e57cb0c4f3317c32
Mary Muensterman <mcmuensterman@yahoo.com>
2022-05-17 21:50:07 -0500

    category controller and learning to spell

---
+++ GameyMcThingy.WebAPI/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GameyMcThingy.Services.Category;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameyMcThingy.WebAPI.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+    }
+}
